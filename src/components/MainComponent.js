f4879abc76f444b99667659b7a34391a506b9a16
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-06-23 17:10:24 +0530

    template

---
+++ src/components/MainComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+function MainComponent() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default MainComponent;
